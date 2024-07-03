8b2e505fa04c12e0e4eb3e8719f4c4b24808d2b8
HughPullman <hughpullman54@gmail.com>
2024-07-03 14:20:23 +0100

    quick fix for delpoyment

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -4,7 +4,7 @@ import { validationResult } from "express-validator";
 export const registerController = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).send(`Unable to login, please check details`);
+        return res.status(422).json({error:`Unable to register, please make sure password is at least 8 characters`});
     }
     try {
         const user = await userService.register(req.body);
